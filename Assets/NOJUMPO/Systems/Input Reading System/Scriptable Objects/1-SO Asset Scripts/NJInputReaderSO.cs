c6e292c336130b03cd12d5b729cd7342854539bf
Berke Konargöçer <berke.konargocer@gmail.com>
2023-09-26 08:26:48 +0300

    Imported my own Input Reading System

---
+++ Assets/NOJUMPO/Systems/Input Reading System/Scriptable Objects/1-SO Asset Scripts/NJInputReaderSO.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Nojumpo.InputSystem
+{
+    [CreateAssetMenu(fileName = "NewInputReader", menuName = "Nojumpo/Scriptable Objects/Game Input/New Input Reader")]
+    public class NJInputReaderSO : ScriptableObject, NJInputActions.IPlayerActions, NJInputActions.IUIActions, NJInputActions.IInspectActions
+    {
+
+#if UNITY_EDITOR
+
+        [TextArea]
+        [SerializeField] string _developerDescription = "CREATE ONE FOR EACH PLAYER (e.g. IF THERE ARE TWO PLAYERS CREATE TWO AND BIND THEIR OWN)";
+
+#endif
+        // -------------------------------- FIELDS ---------------------------------
+        NJInputActions _njInputActions;
+
+        public Vector2 MouseDelta { get; private set; }
+        public Vector2 InspectMouseDelta { get; private set; }
+
+        public Vector2 MoveInput { get; private set; }
+        
+        public delegate void OnMovementInputPressed(Vector2 movementVector);
+        public OnMovementInputPressed onMovementInputPressed;
+        
+        public delegate void OnInteractionInputPressed();
+        public event OnInteractionInputPressed onInteractionInputPressed;
+        
+        public delegate void OnInteractionInputReleased();
+        public event OnInteractionInputReleased onInteractionInputReleased;
+        
+        public delegate void OnJumpInputPressed();
+        public event OnJumpInputPressed onJumpInputPressed;
+        
+        public delegate void OnJumpInputReleased();
+        public event OnJumpInputReleased onJumpInputReleased;
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void OnEnable() {
+            if (_njInputActions == null)
+            {
+                _njInputActions = new NJInputActions();
+
+                _njInputActions.Player.SetCallbacks(this);
+                _njInputActions.UI.SetCallbacks(this);
+                _njInputActions.Inspect.SetCallbacks(this);
+
+                SetPlayerInput();
+            }
+        }
+
+        
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public void OnMove(InputAction.CallbackContext context) {
+            MoveInput = context.ReadValue<Vector2>();
+            onMovementInputPressed?.Invoke(MoveInput);
+        }
+
+        public void OnLook(InputAction.CallbackContext context) {
+            MouseDelta = context.ReadValue<Vector2>();
+        }
+        
+        public void OnInspectLook(InputAction.CallbackContext context) {
+            InspectMouseDelta = context.ReadValue<Vector2>();
+        }
+
+        public void OnInteractButton(InputAction.CallbackContext context) {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                onInteractionInputPressed?.Invoke();
+            }
+
+            if (context.phase == InputActionPhase.Canceled)
+            {
+                onInteractionInputReleased?.Invoke();
+            }
+        }
+
+        public void OnResumeGame(InputAction.CallbackContext context) {
+            // throw new System.NotImplementedException();
+        }
+
+        public void SetPlayerInput() {
+            _njInputActions.Inspect.Disable();
+            _njInputActions.UI.Disable();
+            _njInputActions.Player.Enable();
+        }
+
+        public void SetUIInput() {
+            _njInputActions.Player.Disable();
+            _njInputActions.Inspect.Disable();
+            _njInputActions.UI.Enable();
+        }
+
+        public void SetInspectionInput() {
+            _njInputActions.Player.Disable();
+            _njInputActions.UI.Disable();
+            _njInputActions.Inspect.Enable();
+        }
+    }
+}
