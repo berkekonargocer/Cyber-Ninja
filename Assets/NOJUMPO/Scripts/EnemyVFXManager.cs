f29246454d9e06792f3a65a14d076f1a0e694d90
Berke Konargöçer <berke.konargocer@gmail.com>
2023-10-12 17:16:04 +0300

    EnemyVFXManager created

---
+++ Assets/NOJUMPO/Scripts/EnemyVFXManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class EnemyVFXManager : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+        }
+
+        void OnEnable() {
+        }
+
+        void OnDisable() {
+        }
+
+        void Start() {
+        }
+
+        void Update() {
+        }
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
